d5b8169e0b4257198e33bbc6b71bd2b7300e6e5d
michaeldeven <michael.deven1999@gmail.com>
2022-03-23 09:24:41 +0700

    ngetest silenium boss

---
+++ test_googling.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pytest
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver .get('https://google.co.id')
+    yield driver
+    driver.quit()
+
+
+def test_googling(driver):
+    
+    driver.find_element_by_name('q').send_keys('Michael Deven' + Keys.ENTER)
+    assert 'Michael Deven' in driver.find_element_by_css_selector('h3').text
+    assert 'Michael Deven' in driver.tittle
+    
